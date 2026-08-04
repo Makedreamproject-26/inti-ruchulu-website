88d9ae2238b4baf734920017119994f547a42d1c
nareshgowd <gowd2009@gmail.com>
2026-08-04 15:22:48 +0530

    added files

---
+++ js/data.js
@@ -0,0 +1,101 @@
+const menuItems = [
+
+{
+id:1,
+name:"Karam Dosa",
+price:20,
+category:"Morning",
+image:"images/menu/karam-dosa.jpg"
+},
+
+{
+id:2,
+name:"Masala Dosa",
+price:30,
+category:"Morning",
+image:"images/menu/masala-dosa.jpg"
+},
+
+{
+id:3,
+name:"Ghee Dosa",
+price:30,
+category:"Morning",
+image:"images/menu/ghee-dosa.jpg"
+},
+
+{
+id:4,
+name:"Egg Dosa",
+price:30,
+category:"Morning",
+image:"images/menu/egg-dosa.jpg"
+},
+
+{
+id:5,
+name:"Idly",
+price:10,
+category:"Morning",
+image:"images/menu/idly.jpg"
+},
+
+{
+id:6,
+name:"Lemon Rice",
+price:50,
+category:"Afternoon",
+image:"images/menu/lemon-rice.jpg"
+},
+
+{
+id:7,
+name:"Curd Rice",
+price:50,
+category:"Afternoon",
+image:"images/menu/curd-rice.jpg"
+},
+
+{
+id:8,
+name:"Sambar Rice",
+price:50,
+category:"Afternoon",
+image:"images/menu/sambar-rice.jpg"
+},
+
+{
+id:9,
+name:"Guntha Ponganalu",
+price:40,
+category:"Evening",
+image:"images/menu/ponganalu.jpg"
+},
+
+{
+id:10,
+name:"Boiled Egg",
+price:15,
+category:"Egg",
+image:"images/menu/boiled-egg.jpg"
+},
+
+{
+id:11,
+name:"Omelette",
+price:30,
+category:"Egg",
+image:"images/menu/omelette.jpg"
+},
+
+{
+id:12,
+name:"Egg Bujji",
+price:50,
+category:"Egg",
+image:"images/menu/egg-bujji.jpg"
+}
+
+];
+
+
